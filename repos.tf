086e8cce03793c80845f1deb3b5be27299d4720e
James Harris <contact@jamesharris.id.au>
2022-08-26 13:49:12 +1000

    Add `homebrew-action` repo.

--- repos.tf
+++ repos.tf
@@ -9,3 +9,14 @@ module "repo_grit" {
     since = 2017
   }
 }
+
+module "repo_homebrew_action" {
+  source      = "./modules/repo"
+  name        = "homebrew-action"
+  description = "A GitHub action for publishing Homebrew formulae to private taps."
+  languages   = ["go"]
+
+  copyright = {
+    since = 2022
+  }
+}
